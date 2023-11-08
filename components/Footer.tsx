34605c3d10d22f6de9d21582246bcfddd4aedab8
FilipSankowski <filip.sankowski@gmail.com>
2023-11-08 11:49:37 +0100

    init

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="w-full bg-teal-600 text-white p-4">
+      <span>Autor strony: <br/> Filip Sankowski</span>
+    </div>
+  )
+}
---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="w-full bg-teal-600 text-white p-4">
+      <span>Zadania</span>
+    </div>
+  )
+}
